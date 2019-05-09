19621423c789f8eb3fb27bc6e751016bf9752c92
Therenca <peter.therenca@kenindus.com>
2019-05-09 13:49:15 +0300

    Hello world

---
+++ src/MyComponent.js
@@ -0,0 +1,10 @@
+import React , { Component } from 'react'
+class MyComponent extends Component{
+    render(){
+        return(
+            <div>Hello world!</div>
+        )
+    }
+}
+
+export default MyComponent;
